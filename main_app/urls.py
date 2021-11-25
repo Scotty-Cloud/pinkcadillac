3bfd283c009fcdf037b98eebb6bd5966885190d7
ScottR <astroplexity@gmail.com>
2021-11-24 21:57:09 -0500

    pk's path updated

--- main_app/urls.py
+++ main_app/urls.py
@@ -12,6 +12,10 @@ urlpatterns = [
 
    path('movies/create/', views.MovieCreate.as_view(), name='movies_create'),
 
-   path('movies/<int:pk>/update', views.MovieUpdate.as_view(), name="movies_update") 
+   path('movies/<int:pk>/update', views.MovieUpdate.as_view(), name="movies_update"),
+
+   path('movies/<int:pk>/delete', views.MovieDelete.as_views(), name="movies_delete"),
+   
+    
 
 ]
