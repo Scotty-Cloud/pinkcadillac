8080b11610f90e660e99c64c4ba3d69a4873ec28
ScottR <astroplexity@gmail.com>
2021-11-24 23:03:24 -0500

    add photo assoc path updated

--- main_app/urls.py
+++ main_app/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
   path('songs/<int:pk>/delete/', views.SongDelete.as_views(), name='songs_delete'),
 
 
-  path(),
+  path('movies/<int:movie_id>/add_photo/', views.add_photo, name="add_photo"),
 
   path("accounts/signup/", views.signup, name="signup")
 ]
