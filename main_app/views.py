d048c45beac3d283a9beb764fb4de2ec8f37e57d
ScottR <astroplexity@gmail.com>
2021-11-26 15:26:00 -0500

    user signUp added

--- main_app/views.py
+++ main_app/views.py
@@ -75,5 +75,19 @@ class SongDelete(LoginRequiredMixin, DeleteView):
   model = Song
   success_url = '/songs/'
 
+@login_required
 
+def signup(request):
+  error_message = ""
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('movies_index')
+    else:
+      error_message = "Invalid sign up try again"
+  form = UserCreationForm()
+  context = {'form' : form, 'error_message' : error_message}
+  return render(request, 'signup.html', context)
 
