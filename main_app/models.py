1e13e097eeb54a776c1e759c311c9d5c2ed4e22d
ScottR <astroplexity@gmail.com>
2021-11-26 15:40:32 -0500

    movie models added

--- main_app/models.py
+++ main_app/models.py
@@ -12,4 +12,23 @@ ENJOY = (
 class Song(models.Model):
   name = models.CharField(max_length = 25)
 
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('songs_detail', kwargs={'pk' : self.id})
+  
 # Create your models here.
+class Movie(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.CharField(max_length=250)
+  recommend = models.CharField(max_length=25)
+  songs = models.ManyToManyField(Song)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse("movies_detail", kwargs={"pk": self.id})
+  
