1d477721d33cd8eee02ad334c554d189a6f5e92d
ScottR <astroplexity@gmail.com>
2021-12-01 00:15:25 -0500

    edit about page

--- main_app/static/js/movie-detail.js
+++ main_app/static/js/movie-detail.js
@@ -1,17 +1,5 @@
 const dateInput = document.getElementById("id_date")
 
-// const fileInput = document.getElementById('file-input')
-// const fileName = document.getElementById('file-name')
-
-// fileInput.addEventListener('change', evt => {
-//   const fileToUpload = evt.target.files[0].name
-//   if(fileToUpload) {
-//     fileName.innerText = fileToUpload
-//   } else {
-//     fileName.innerText = ""
-//   }
-// })
-
 const picker = MCDatepicker.create({
   el: "#id_date",
   dateFormat: "yyyy-mm-dd",
